3dd18b2b61f168bd2f299fa97ffa170f4a358fb1
LeonardoMonti <LeonardoMonti>
2022-10-21 13:43:36 -0300

    feat: func get unique job industries

--- src/insights.py
+++ src/insights.py
@@ -25,22 +25,12 @@ def filter_by_job_type(jobs, job_type):
     return []
 
 
-def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+def get_unique_industries(path: str):
+    get_read = read(path)
+    unique_job_industries = set(
+        [job["industry"] for job in get_read if ["industry"] != ""]
+    )
+    return unique_job_industries
 
 
 def filter_by_industry(jobs, industry):
