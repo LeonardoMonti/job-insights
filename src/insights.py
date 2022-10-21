904ddb9c742cae345018df94d8af0d2b7f81ea36
LeonardoMonti <LeonardoMonti>
2022-10-21 15:15:11 -0300

    feat: func filter jobs

--- src/insights.py
+++ src/insights.py
@@ -7,22 +7,13 @@ def get_unique_job_types(path: str):
     return unique_job_type
 
 
-def filter_by_job_type(jobs, job_type):
-    """Filters a list of jobs by job_type
+def filter_by_job_type(jobs: list, job_type: str):
+    if job_type == "":
+        return []
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
+    filtered = [job for job in jobs if job["job_type"] == job_type]
 
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    return []
+    return filtered
 
 
 def get_unique_industries(path: str):
